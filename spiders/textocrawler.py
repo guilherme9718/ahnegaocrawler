e0538d8ed1fc179d49ff21e36aa13f380507b9cb
Guilherme Aguilar <guilhermeaguilarguii@gmai.com>
2021-02-15 19:30:05 -0300

    melhorado o scraping de texto e adcionado uma nova função

--- spiders/textocrawler.py
+++ spiders/textocrawler.py
@@ -1,26 +1,33 @@
 import scrapy
 import pandas as pd
 import re
-import os.path
 
-PAGES = 6
+PAGES = 49
 PAGE_URL = 'https://www.ahnegao.com.br/'
+PAGE_ALT = 'https://www.ahnegao.com.br/t/coletanea-de-videos-bestas/'
 
 
 class TextCrawler(scrapy.Spider):
     name = 'ahnegao_texto_spider'
-    start_urls = [PAGE_URL] + [str(PAGE_URL + 'pag/') + str(page_num) for page_num in range(2, 2 + PAGES)]
-    
+    start_urls = [PAGE_ALT] + [str(PAGE_ALT + 'pag/') + str(page_num) for page_num in range(2, 2 + PAGES)]
+    loop = 0
+    page = 1
+        
     def parse(self, response):
         # taking all the url of blog posts
+        print('page = ', self.page)
+        self.page += 1
         contents = response.css('div.post-content')
         for content in contents:
+            title = content.css('h2.entry-title a::text').extract_first()
+            title = ''.join(title).strip()
             url = content.css('.entry-title a::attr(href)').get()
-            print(url)
             yield response.follow(url, callback = self.getContent)
             
-    def getContent(self,response):
+    def getContent(self, response):
         # in each blog post take all text
+        self.loop += 1
+        
         title = response.css('h1.entry-title::text').extract_first()
         title = ''.join(title).strip()
         
@@ -28,17 +35,15 @@ class TextCrawler(scrapy.Spider):
         date = ''.join(date).strip()
         
         text = response.css('div.entry-content p::text').extract()
-        for string in text:
-            string = string.replace('\n', '.')
-            string = re.sub('\W+',' ',string).strip()
-        text_completed = '. '.join(text)
+        text = [x for x in text if x != u'\xa0']
+        text_completed = ' \n'.join(text)
         
         data_dict = {'post':title, 'data':date, 'texto':text_completed}
         df = pd.DataFrame(data=data_dict, index=[0])
-        if os.path.isfile('textos_ahnegao.csv'):
-            h = False
-            m = 'a'
-        else:
+        if (self.loop == 1):
             h = True
             m = 'w'
+        else:
+            h = False
+            m = 'a'
         yield df.to_csv(path_or_buf='textos_ahnegao.csv', index=False, header=h, mode=m)
